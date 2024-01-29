e58ee60ea1e100b3d7667dd9729ad675fb3ad304
Payam <p.hoseini.ch@gmail.com>
2024-01-29 20:32:15 +0330

    Complete journal page, make two components for that.

---
+++ components/NewEntry.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { revalidatePath } from "next/cache";
+import { useRouter } from "next/navigation";
+
+const NewEntry = () => {
+  const router = useRouter();
+
+  return (
+    <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5 sm:p-6">
+        <span className="text-3xl">New Entry</span>
+      </div>
+    </div>
+  );
+};
+
+export default NewEntry;
